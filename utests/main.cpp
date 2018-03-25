f89bdb305d255aa2315c34dd27ebe8e8c3a81fe7
Adrien Oliva <olivaa+gitlab@yapbreak.fr>
2018-03-25 13:51:21 +0200

    Add stub test

--- utests/main.cpp
+++ utests/main.cpp
@@ -1,6 +1,7 @@
 #include <map>
 
 #include <CppUTest/CommandLineTestRunner.h>
+#include "ut_button.h"
 
 
 int main(int argc, const char *argv[])
---
+++ utests/ut_button.h
@@ -0,0 +1,16 @@
+#ifndef UT_BUTTON_H_VPZWRLC7
+#define UT_BUTTON_H_VPZWRLC7
+
+#include <CppUTest/TestHarness.h>
+#include "button.h"
+#include "Arduino.h"
+
+TEST_GROUP(basic)
+{
+};
+
+TEST(basic, nothing)
+{
+};
+
+#endif /* end of include guard: UT_BUTTON_H_VPZWRLC7 */
